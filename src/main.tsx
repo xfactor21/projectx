679408d9f793b5defc6500bdd02fdd413014eaff
xFactor <xfactorxai@gmail.com>
2026-08-18 15:53:20 -0500

    Wrap app with runtime error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ErrorBoundary from './ErrorBoundary.tsx'
 import './viewModes.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
