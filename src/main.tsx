5f56d5314c7e010eb48b86bddf4a71683e4672a2
xFactor <xfactorxai@gmail.com>
2026-08-21 16:41:17 -0500

    Companion: expose mobile client mode

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import LocalProjectDock from './LocalProjectDock.tsx'
 import ProjectIntelDock from './ProjectIntelDock.tsx'
 import DeploymentAnalyticsDock from './DeploymentAnalyticsDock.tsx'
 import DeployDock from './DeployDock.tsx'
+import CompanionApp from './CompanionApp.tsx'
 import './viewModes.css'
 import './phase3.css'
 import './cloudSync.css'
@@ -18,17 +19,22 @@ import './localProjects.css'
 import './projectIntel.css'
 import './deploymentAnalytics.css'
 import './deployDock.css'
+import './companion.css'
+
+const companionMode = new URLSearchParams(window.location.search).get('mode') === 'companion'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
-      <CloudSyncDock />
-      <LocalProjectDock />
-      <ProjectIntelDock />
-      <DeploymentAnalyticsDock />
-      <DeployDock />
-      <GitHubDiscoveryModal />
+      {companionMode ? <CompanionApp /> : <>
+        <App />
+        <CloudSyncDock />
+        <LocalProjectDock />
+        <ProjectIntelDock />
+        <DeploymentAnalyticsDock />
+        <DeployDock />
+        <GitHubDiscoveryModal />
+      </>}
     </ErrorBoundary>
   </StrictMode>,
 )
