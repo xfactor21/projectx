ec8f00dedee144f733f951c8539d6b0700405a3f
xFactor <xfactorxai@gmail.com>
2026-08-21 16:37:29 -0500

    Local projects: mount folder onboarding controls

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,19 @@ import App from './App.tsx'
 import ErrorBoundary from './ErrorBoundary.tsx'
 import CloudSyncDock from './CloudSyncDock.tsx'
 import GitHubDiscoveryModal from './GitHubDiscoveryModal.tsx'
+import LocalProjectDock from './LocalProjectDock.tsx'
 import './viewModes.css'
 import './phase3.css'
 import './cloudSync.css'
 import './githubDiscovery.css'
+import './localProjects.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
       <CloudSyncDock />
+      <LocalProjectDock />
       <GitHubDiscoveryModal />
     </ErrorBoundary>
   </StrictMode>,
