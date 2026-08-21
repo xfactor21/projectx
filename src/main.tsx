e8afe8f5f36f28c65e621a7d0b1cc62df382a2bd
xFactor <xfactorxai@gmail.com>
2026-08-21 16:38:04 -0500

    Intelligence: mount project attention scanner

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import ErrorBoundary from './ErrorBoundary.tsx'
 import CloudSyncDock from './CloudSyncDock.tsx'
 import GitHubDiscoveryModal from './GitHubDiscoveryModal.tsx'
 import LocalProjectDock from './LocalProjectDock.tsx'
+import ProjectIntelDock from './ProjectIntelDock.tsx'
 import './viewModes.css'
 import './phase3.css'
 import './cloudSync.css'
 import './githubDiscovery.css'
 import './localProjects.css'
+import './projectIntel.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -19,6 +21,7 @@ createRoot(document.getElementById('root')!).render(
       <App />
       <CloudSyncDock />
       <LocalProjectDock />
+      <ProjectIntelDock />
       <GitHubDiscoveryModal />
     </ErrorBoundary>
   </StrictMode>,
