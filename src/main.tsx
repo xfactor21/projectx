8ff4aec40821348c762162a5b516899da42afe47
xFactor <xfactorxai@gmail.com>
2026-08-21 12:22:05 -0500

    GitHub: mount selective discovery review flow

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import './errorBoundary.css'
 import App from './App.tsx'
 import ErrorBoundary from './ErrorBoundary.tsx'
 import CloudSyncDock from './CloudSyncDock.tsx'
+import GitHubDiscoveryModal from './GitHubDiscoveryModal.tsx'
 import './viewModes.css'
 import './phase3.css'
 import './cloudSync.css'
+import './githubDiscovery.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
       <CloudSyncDock />
+      <GitHubDiscoveryModal />
     </ErrorBoundary>
   </StrictMode>,
 )
