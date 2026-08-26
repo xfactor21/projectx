7473a467757f01ede028ed332ba09c58ed13f5ff
xFactor <xfactorxai@gmail.com>
2026-08-25 20:36:48 -0500

    mobile: bump project.X shared version to v1.7

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.6'
-export const APP_SEMVER = '1.6.0'
-export const BUILD_DATE_TAG = '8-24'
+export const APP_VERSION = '1.7'
+export const APP_SEMVER = '1.7.0'
+export const BUILD_DATE_TAG = '8-25'
 export const BUILD_BASENAME = `projectX_${BUILD_DATE_TAG}_v${APP_VERSION}`
